34bc542139d42dbd266b5d65e26d38ccc280318d
Jocelyn Tonnelier <jocelyn.tonnelier@gmail.com>
2016-01-13 15:18:26 +0100

    Response and gitignore

---
+++ sgbdService/src/main/java/com/responses/UserConnexionResponse.java
@@ -0,0 +1,21 @@
+package main.java.com.responses;
+
+/**
+ * Created by Jocelyn on 13/01/2016.
+ */
+public class UserConnexionResponse {
+
+    private int isConnect;
+
+    public UserConnexionResponse(){
+
+    }
+
+    public int isConnect() {
+        return isConnect;
+    }
+
+    public void setIsConnect(int isConnect) {
+        this.isConnect = isConnect;
+    }
+}
