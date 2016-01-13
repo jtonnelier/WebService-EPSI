044d95950908a03fecd655bb83db14b61c93e280
Jocelyn Tonnelier <jocelyn.tonnelier@gmail.com>
2016-01-13 16:49:46 +0100

    Conf WebService SGBD

--- sgbdService/src/main/java/com/SgbdServiceAPI.java
+++ sgbdService/src/main/java/com/SgbdServiceAPI.java
@@ -1,5 +1,6 @@
 package main.java.com;
 
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.Collections;
 import java.util.Set;
@@ -7,6 +8,7 @@ import java.util.Set;
 /**
  * Created by Jocelyn on 13/01/2016.
  */
+@ApplicationPath("")
 public class SgbdServiceAPI extends Application {
     public Set<Class<?>> getClasses() {
         return Collections.emptySet();
